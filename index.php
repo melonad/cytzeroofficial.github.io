4ae4d66b7c35a93018daed0664d417f7fe23b9b6
melonad <barbutaradv@gmail.com>
2024-09-03 00:14:56 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * @brief		Public bootstrap
+ * @brief		Admin CP bootstrap
  * @author		<a href='https://www.invisioncommunity.com'>Invision Power Services, Inc.</a>
  * @copyright	(c) Invision Power Services, Inc.
  * @license		https://www.invisioncommunity.com/legal/standards/
  * @package		Invision Community
  * @since		18 Feb 2013
- */ 
+ */
+
 \define('REPORT_EXCEPTIONS', TRUE);
-$_SERVER['SCRIPT_FILENAME']	= __FILE__;
-require_once 'init.php';
-\IPS\Dispatcher\Front::i()->run();
+require_once '../init.php';
+\IPS\Dispatcher\Admin::i()->run();
